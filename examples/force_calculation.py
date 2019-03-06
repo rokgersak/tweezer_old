b81459c35183341f4ee37eb5d588d08c443cd027
andrej5elin <andrej.petelin@ijs.si>
2019-03-06 09:43:55 +0100

    Force calculation packaging
    Make last merge more pythonic. Move example to examples, renamed filenames from CamelCase to under_score

--- examples/force_calculation.py
+++ examples/force_calculation.py
@@ -1,11 +1,12 @@
-import ForceCalc as forcecalc
-import SynthActiveTrajectory as sat
+import tweezer.force_calc as forcecalc
+import tweezer.synth_active_trayectory as sat
 
 #   A minimal workflow example; trap stiffnesses in tuple "kValues" need to be provided.
 
 kValues = (2.5e-6,0.5e-6)
-values = forcecalc.read_input("test.dat")
+
 sat.SAT2("test.dat",1000,0.005, kValues[0], kValues[1], 2, 1, 1e-6, 1e-6, 0.5e-6, 9.7e-4, 300, 2)
+values = forcecalc.read_input("test.dat")
 
 #   In this case, the calculated values are not recorded.
 
