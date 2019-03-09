517203cb205ba5be3558affe7343ac8958bf3cda
katjagosar <47926449+katjagosar@users.noreply.github.com>
2019-03-09 14:34:27 +0100

    First version of calibration test
    Tests for functions:
    center_and_rotate, subtract_running_average and calibrate.
    The tested data is generated by generate_calibration_data.
    Using this I noticed that the center_and rotate function does not work correctly. The calculated rotation angles are typically too small.

--- tweezer/test/test_calibration.py
+++ tweezer/test/test_calibration.py
@@ -3,17 +3,51 @@
 import unittest
 
 import numpy as np
-import tweezer.calibration as calib
+#import calibration
+import tweezer.calibration as calibration
+import generate_calibration_data
 
 class TestCalibration(unittest.TestCase):
     
     def setUp(self):
-        self.parameter = 1.
-        self.expected_result = np.array([2.,3.])
+        self.kx = 1e-6
+        self.ky = 2e-6
+        self.phi = 0 
+        self.expected_result = np.array([tuple([self.kx, self.ky]), self.phi])
         
-    def test_function1(self):
-        result = calib.function1((1,2),b = 1)
-        self.assertTrue(np.allclose(result, self.expected_result)) 
+    def test_center_and_rotate(self):
+        data =  generate_calibration_data.generate(
+                [self.kx, self.ky], phi = self.phi
+                )
+        result = calibration.center_and_rotate(
+                        data[1], data[2]
+                        )
+        self.assertTrue(np.allclose(result[2], self.expected_result[1], 
+                                    atol = 0.1))
+        
+    def test_running_average(self):
+        data =  generate_calibration_data.generate(
+                [self.kx, self.ky], phi = self.phi
+                )
+        x = calibration.subtract_running_average(data[0], data[1])
+        y = calibration.subtract_running_average(data[0], data[2])
+        self.assertTrue(
+                np.allclose(np.mean(x), 0, atol = 1e-3) and
+                np.allclose(np.mean(y), 0, atol = 1e-3)
+                )
+        #We could also check the standard deviation.
+        
+    def test_calibrate(self):
+        data =  generate_calibration_data.generate(
+                [self.kx, self.ky], phi = self.phi
+                )
+        result = calibration.calibrate(
+                        data[0], data[1], data[2]
+                        )
+        self.assertTrue(
+                np.allclose(result[0], self.expected_result[0], atol=1e-6) and
+                np.allclose(result[1], self.expected_result[1], atol=0.1))
+        
 
 if __name__ == "__main__":
     unittest.main()
